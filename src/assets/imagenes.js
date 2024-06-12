46d734d5ad3d90636ec6f273fe327e4ba5380c1c
Luquitas234 <Clastiones@gmail.com>
2024-06-12 10:53:04 -0300

    Create imagenes.js

---
+++ src/assets/imagenes.js
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-anonymous-default-export */
+import aloja from "./img/1.webp";
+import aloja2 from "./img/2.webp";
+import user from "./img/usuario.png";
+import aloja3 from "./img/3.webp"
+import aloja4 from "./img/primera.webp";
+import aloja5 from "./img/segunda.webp";
+import aloja6 from "./img/tercera.webp";
+import aloja7 from "./img/carousell1.jpeg";
+import aloja8 from "./img/carousell2.jpg";
+import aloja9 from "./img/carousell3.jpeg";
+import aloja10 from "./img/Viaje.png";
+import aloja11 from "./img/LogoFull.png";
+import aloja12 from  "./img/Logo2.jpeg";
+import aloja13 from "./img/Promo.webp";
+
+
+export default {
+    "img1": aloja,
+    "img2": aloja2,
+    "img3": aloja3,
+    "img4": user,
+    "img5":aloja4,
+    "img6":aloja5,
+    "img7":aloja6,
+    "img8":aloja7,
+    "img9":aloja8,
+    "img10":aloja9,
+    "img11":aloja10,
+    "img12":aloja11,
+    "img13":aloja12,
+    "img14":aloja13
+
+}
